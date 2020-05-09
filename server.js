52b9262bf1898cfb1c5fe71ea956f61b7e91d657
Joao <jpmail98@gmail.com>
2020-05-09 01:05:32 -0300

    worksfuck

--- server.js
+++ server.js
@@ -3,12 +3,7 @@ var port = process.env.PORT || 5000;
 const express = require('express');
 const app = express();
 
-app.use(express.static('.'));
-
-app.get('*', function(req,res){
-    res.send(`<div style="text-align:center;"><h2>ERRO 404</h2><h3>NÃO ENCONTRADO!<h3></div>`);
-    console.log('404')
-})
+app.use(express.static('build'));
 
 console.log(`Server Running on port ${port} !`);
 app.listen(port);
